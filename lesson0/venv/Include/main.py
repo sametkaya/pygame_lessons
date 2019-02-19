5fc847104f891ca8c91f3436e25a5cbfa9882917
samet <skaya@fsm.edu.tr>
2019-02-19 14:46:27 +0300

    lesson0

---
+++ lesson0/venv/Include/main.py
@@ -0,0 +1,25 @@
+
+print("merhaba")
+x=5
+
+print(type(x))
+x="fsm"
+print(type(x))
+#x=x+5
+#print(x)
+
+#x=x+y
+#print(x)
+
+y=5
+
+if y==5:
+    print("eşit")
+    if y>3:
+        print("y üçten büyüktür")
+    print("y üçten büyüktür")
+    print("y üçten büyüktür")
+    print("y üçten büyüktür")
+else:
+    print("eşit değil")
+
